90e0bebaa7c47bbd8fa854b1c82f328178bdf774
THAXTERIA <39074621+THAXTERIA@users.noreply.github.com>
2019-09-26 19:56:13 -0400

    fix the repo

---
+++ Assets/Src/PaddleBehaviours/PlayerInteractable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerInteractable : MonoBehaviour
+{
+	public abstract void OnHit(ControllerColliderHit hit, Player player);
+}
