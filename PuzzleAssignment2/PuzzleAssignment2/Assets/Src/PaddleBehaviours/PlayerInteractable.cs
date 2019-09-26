2540ece6129102ff8709eea480be8d7d702a0707
THAXTERIA <39074621+THAXTERIA@users.noreply.github.com>
2019-09-26 19:55:36 -0400

    Attempting to fix the repo folder to track gitignores

--- PuzzleAssignment2/PuzzleAssignment2/Assets/Src/PaddleBehaviours/PlayerInteractable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlayerInteractable : MonoBehaviour
-{
-	public abstract void OnHit(ControllerColliderHit hit, Player player);
-}
